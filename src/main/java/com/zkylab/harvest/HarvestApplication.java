2d12f8a2b265ccd05d230f77fa39efe4a4b36e8f
zyrridian <rezkyaf246@gmail.com>
2025-10-11 13:15:28 +0700

    initial commit

---
+++ src/main/java/com/zkylab/harvest/HarvestApplication.java
@@ -0,0 +1,13 @@
+package com.zkylab.harvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarvestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zkylab/harvest/HarvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkylab.harvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
